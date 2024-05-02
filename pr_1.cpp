b56fe0c27a2a58c7a710c6c4f9b279e08980ecb3
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2024-05-02 21:30:48 +0530

    updation

--- pr_1.cpp
+++ pr_1.cpp
@@ -8,7 +8,7 @@ using namespace std ;
 
 int64_t getSum(int n){
     int64_t itr = 0 , sum {};
-    while(++itr < n){
+    while(++itr <= n){
         if(itr%3 ==0 || itr%5 ==0)
             sum+=itr ;
     }
@@ -24,15 +24,20 @@ int64_t getSum(int n){
 n/2 * (a1 + an )
 
 */
+
 int64_t constSumget(int limit , int num1 , int num2){
-    int num3 = num1 * num2 ;
-    int n1 , n2 , n3;
+    int64_t num3 = num1 * num2 ;
+    int64_t n1 , n2 , n3;
 
     n1 = limit /num1 ;
     n2 = limit /num2 ;
     n3 = limit / num3 ;
 
-    int sum1 = ()
+    int64_t sum1 = (n1 * (num1 + n1*num1))>>1;
+    int64_t sum2 = (n2 * (num2 + n2*num2))>>1;
+    int64_t sum3 = (n3 * (num3 + n3*num3))>>1;
+
+    return sum1 + sum2 - sum3 ;
 
 }
 
@@ -42,9 +47,11 @@ int main(int argc , char const *argv[]){
     int n ;
     std::cin>> n ;
 
-    cout<<"\nsum : "<<getSum(n) <<endl ;
+    cout<<"\nsum : "<<getSum(n-1) <<endl ;
 
+    cout<<"\n ,...\n";
 
+    cout<<"\n s : "<<constSumget(n-1 , 3 , 5) ;
 
     // std::cin.get();
     return 0;
